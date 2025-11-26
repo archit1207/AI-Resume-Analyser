abb6c0f221e354070a27a75a4d88916ee4537116
archit1207 <architmishra1207@gmail.com>
2025-11-26 16:04:09 +0530

    Page Title

--- app/routes/wipe.tsx
+++ app/routes/wipe.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 import { usePuterStore } from "~/lib/puter";
+import type { Route } from "../+types/root";
 
 export function meta({ }: Route.MetaArgs) {
     return [
